d068ede703c749891789eef2a5857b3cd138ae74
kimhanju <khj64076407@gmail.com>
2021-04-22 19:22:17 +0900

    video

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,30 @@
-export const join = (req, ress) => ress.render("join", { pageTitle: "Join" });
-export const login = (req, ress) =>
-  ress.render("login", { pageTitle: "Login" });
-export const logout = (req, ress) =>
-  ress.render("logout", { pageTitle: "Logout" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+export const logout = (req, res) => {
+  // To Do: process Log Out
+  res.render(routes.home, { pageTitle: "Logout" });
+};
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
